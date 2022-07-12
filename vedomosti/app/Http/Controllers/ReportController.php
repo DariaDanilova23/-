119452e258bc8d5ee6a30cac24450078b12b14b5
DariaDanilova23 <danilowa.daria2010@mail.ru>
2022-07-13 02:35:25 +0300

    activeCourse

--- vedomosti/app/Http/Controllers/ReportController.php
+++ vedomosti/app/Http/Controllers/ReportController.php
@@ -6,6 +6,7 @@ use App\Models\ActiveCourse;
 use App\Models\Course;
 use App\Models\Professor;
 use App\Models\Report;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -30,65 +31,46 @@ class ReportController extends Controller
     public function professor (){
         return view('professors');
     }
+
+    public function bestStudent()
+    {
+        Report::truncate();
+        $i=0;
+        foreach (ActiveCourse::with('student')->get() as $record) {
+            $row = new Report;//---
+            $best= array();
+            $st=new ActiveCourse();
+            $row->FIO=$record['student']['FIO'];//---
+            $best[$i]['FIO']=$record['student']['FIO'];
+            $row->Amount=0;//----
+            $row->Average_grade = $st->average($record);//-------
+            $best[$i]['grade']=$st->average($record);
+            $row->save();//-------
+            $i++;
+        }
+        return $this->index();
+    }
     public function create()
     {
         Report::truncate();
-        foreach ($records=Professor::with('activeCourse')->get() as $record) {
-            $row = new Report;
-            $marks_sum=0;
-            $amount=0;
-            $row->FIO=$record['FIO'];
-            foreach ($record['activeCourse'] as $marks){
-                $marks_sum+=array_sum($marks['grade']);
-                $amount+=count($marks['grade']);
-            }
-            if($amount==0) $row->Average_grade=0;
-            else $row->Average_grade = $marks_sum/$amount;
-            $row->Amount = count($record['activeCourse']);
-            $row->save();
-        }
-      /*
-        ///самое ок
-        Report::truncate();
-        foreach ($records=ActiveCourse::with('course','student')->get() as $record) {
-            $row = new Report;
-            $row->FIO = $record;
-            $row->Average_grade = 0;
-            $row->Amount = 0;
-            $row->save();
-        }
-        ///*/
-       /* foreach (ActiveCourse::with('course')->get() as $professors){
-            $row = new Report;
-            $row->FIO=$professors['id'];
-            $row->Average_grade=0;
-            $row->Amount=0;
-            $row->save();
-        }*/
-       /* Report::truncate();
-        foreach (Professor::all() as $professors){
-            $row = new Report;
-            $countMarks=0;
-            $sumMarks=0;
+        $i=0;
+        foreach (Professor::with('activeCourse')->get() as $record) {
+            $row = new Report;//-----------
+            $reportActive= array();
+            $st=new ActiveCourse();
+            $reportActive[$i]['FIO']=$record['FIO'];
+            $row->FIO=$reportActive[$i]['FIO'];//-------
             $average=0;
-            $row->FIO=$professors['FIO'];
-            $course=Course::all()->where('id_professor',$professors['id']);
-            $sudentsOnCourse=0;
-            foreach ($course as $courseItem){ //проход по каждому курсу преподавателя в таблице Course
-                $activeCourse=ActiveCourse::all()->where('id_course',$courseItem['id']);
-                $sudentsOnCourse+=$activeCourse->count();
-                foreach ($activeCourse as $student) { //проход по каждому курсу преподавателя в таблице ActiveCourse
-                    $sumMarks+=array_sum($student['grade']);
-                    $countMarks+=count($student['grade']);
-                    }
-                if($countMarks!=0) {
-                    $average = $sumMarks / $countMarks;//среднее арифметическое
-                }
+            foreach ($record['activeCourse'] as $mark){
+                $average+=$st->average($mark);
             }
-            $row->Amount=$sudentsOnCourse;
-            $row->Average_grade=$average;
-            $row->save();
-        }*/
+            $reportActive[$i]['grade']=$average;
+            $row->Average_grade=$reportActive[$i]['grade'];//------------
+            $reportActive[$i]['amount']=count($record['activeCourse']);
+            $row->Amount=$reportActive[$i]['amount'];//-----
+            $row->save();//-----------
+            $i++;
+        }
         return $this->index();
     }
 
--- vedomosti/app/Models/ActiveCourse.php
+++ vedomosti/app/Models/ActiveCourse.php
@@ -20,4 +20,12 @@ class ActiveCourse extends Model
     public function course(){
         return $this->belongsTo(Course::class,'id_course','id');
     }
+    public function average($id){
+        $mark=0;
+        $amount=0;
+            $mark+=array_sum($id['grade']);
+            $amount+=count($id['grade']);
+        if($amount==0) return 0;
+        else return $mark/$amount ;
+    }
 }
